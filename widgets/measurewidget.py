3ea51cc16ebebced66fb99ac44e7b4d0a58e93b1
derElektrobesen <pberejnoy2005@gmail.com>
2014-01-08 09:17:54 +0400

    Properties added

--- widgets/measurewidget.py
+++ widgets/measurewidget.py
@@ -5,20 +5,41 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.legend_handler import HandlerLine2D
 from matplotlib.lines import Line2D
 
-class MeasureWidget(FigureCanvas):
-    def __init__(self, parent = None):
-        self.__fig = Figure()
-        self.__axes = self.__fig.add_subplot(111)
+def _composed(*decs):
+    def deco(f):
+        for dec in reversed(decs):
+            f = dec(f)
+        return f
+    return deco
+
+_st_prop = _composed(staticmethod, property)
+
+class __Params:
+    @_st_prop
+    def red_int_start(): return 6600.0
+
+    @_st_prop
+    def red_int_end(): return 8000.0
+
+    @_st_prop
+    def a1_start(): return 6650.0
+
+    @_st_prop
+    def a1_end(): return 6750.0
+
+    @_st_prop
+    def a2_start(): return 6950.0
+
+    @_st_prop
+    def a2_end(): return 7050.0
+
+class SpectorsCollection:
+    def __init__(self):
         self.__graphs = {}
-        FigureCanvas.__init__(self, self.__fig)
-        self.setParent(parent)
 
     def add_graph(self, key, graph):
+        graph = self.__process_graph_bounds(graph)
         self.__graphs[key] = { 'graph': graph, }
-
-    def set_graph(self, key, graph):
-        self.clear()
-        self.add_graph(key, graph)
 
     def remove_graph(self, key):
         del self.__graphs[key]
@@ -26,8 +47,25 @@ class MeasureWidget(FigureCanvas):
     def clear(self):
         self.__graphs = {}
 
+    def set_graph(self, key, graph):
+        self.clear()
+        self.add_graph(key, graph)
+
+    def graphs(self):
+        return self.__graphs
+
+    def __process_graph_bounds(self, gr):
+        return gr
+
+class MeasureWidget(FigureCanvas, SpectorsCollection):
+    def __init__(self, parent = None):
+        self.__fig = Figure()
+        self.__axes = self.__fig.add_subplot(111)
+        FigureCanvas.__init__(self, self.__fig)
+        self.setParent(parent)
+
     def set_color(self, key, color):
-        if key in self.__graph:
+        if key in self.graphs():
             self.__graph[key]['color'] = color
 
     def __calculate_graph(self, gr):
@@ -36,7 +74,7 @@ class MeasureWidget(FigureCanvas):
     def render(self):
         plt = self.__axes
         plt.cla()
-        for gr in self.__graphs.values():
+        for gr in self.graphs().values():
             line = plt.plot(*(gr['graph'].get_data()))
             if 'color' in gr:
                 plt.setp(line, color = gr['color'])
--- widgets/measurewidget_children.py
+++ widgets/measurewidget_children.py
@@ -1,56 +1,67 @@
-from .measurewidget_parents import *
 from PyQt4.QtGui import *
+from .measurewidget_parents import *
+from .measurewidget import SpectorsCollection
 
-class BlueSpW(MainSpW):
+class BlueCollection(SpectorsCollection):
+    def __process_graph_bounds(self, gr):
+        print("Blue")
+        return gr
+
+class RedCollection(SpectorsCollection):
+    def __process_graph_bounds(self, gr):
+        print("Red")
+        return gr
+
+class BlueSpW(MainSpW, BlueCollection):
     def __init__(self, parent = None):
         MainSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class RedSpW(MainSpW):
+class RedSpW(MainSpW, RedCollection):
     def __init__(self, parent = None):
         MainSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class OrigBlueSpW(OrigSpW):
+class OrigBlueSpW(OrigSpW, BlueCollection):
     def __init__(self, parent = None):
         OrigSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class OrigRedSpW(OrigSpW):
+class OrigRedSpW(OrigSpW, RedCollection):
     def __init__(self, parent = None):
         OrigSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class DiffBlueSpW(DiffSpW):
+class DiffBlueSpW(DiffSpW, BlueCollection):
     def __init__(self, parent = None):
         DiffSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class DiffRedSpW(DiffSpW):
+class DiffRedSpW(DiffSpW, RedCollection):
     def __init__(self, parent = None):
         DiffSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class IntactBlueSpW(IntactSpW):
+class IntactBlueSpW(IntactSpW, BlueCollection):
     def __init__(self, parent = None):
         IntactSpW.__init__(self, parent)
 
     def __calculate_graph(self, gr):
         return gr
 
-class IntactRedSpW(IntactSpW):
+class IntactRedSpW(IntactSpW, RedCollection):
     def __init__(self, parent = None):
         IntactSpW.__init__(self, parent)
 
