8628ef86206eb63d5024a9c105f107b7f9c3ca87
derElektrobesen <pberejnoy2005@gmail.com>
2014-01-12 19:31:58 +0400

    Treatments importing impemented

--- data/filter_cards.pl
+++ data/filter_cards.pl
@@ -10,6 +10,9 @@ use utf8;
 open my $f, ">:encoding(UTF-8)", "results.sql";
 open my $inf, "<:encoding(UTF-8)", "dump.sql";
 my %names;
+my @visits;
+
+print $f "use `spectro_viewer`;\nstart transaction;\n\n";
 
 while (<$inf>) {
     if (/insert.*names.*\((\d+),'([^']+)','([^']+)','([^']+)','([^']+)'\)/i) {
@@ -28,6 +31,33 @@ while (<$inf>) {
             treat       => $5,
             %{$names{$1} || {}}
         };
+    } elsif (/insert.*diagrams.*\((\d+),(\d+),(\d+),'([^']+)','([^']+)',(\d+)\)/i) {
+        my $rec = {
+            dia_id  => $2,
+            dev     => $3 eq '1' ? "blue" : "red",
+            point   => $4,
+            type    => $4 eq '1' ? "intact" : "other",
+        };
+        if ($visits[$6]) {
+            push @{$visits[$6]->{diagrams}}, $rec;
+        } else {
+            $visits[$6] = {
+                name_id => $1,
+                date    => $5,
+                diagrams=> [$rec],
+            };
+        }
+    } elsif (/insert.*comingrecords.*\((\d+),\d+,(\d+),([^,]+),(\d+),(\d+),'([^']*)','([^']*)','([^']*)'\)/i) {
+        $visits[$1] = {
+            %{$visits[$1]},
+            circle_day  => $2,
+            endo_w      => $3,
+            scar        => $4,
+            fibro       => $5,
+            onco        => $6,
+            more        => $7,
+            treat       => $8,
+        };
     }
 }
 
@@ -36,7 +66,10 @@ my $i = 0;
 
 for my $key (keys %names) {
     my $cur = $names{$key};
-    next unless $cur->{name} and $cur->{middlename} and $cur->{lastname};
+    unless ($cur->{name} and $cur->{middlename} and $cur->{lastname}) {
+        delete $names{$key};
+        next;
+    }
     my $card = $cur->{card_no} && uc $cur->{card_no};
     if (defined $card && $card =~ /([А-Я])(?:-| )?(\d{4})/) {
         $card = "$1-$2";
@@ -49,6 +82,22 @@ for my $key (keys %names) {
             $cur->{key} = ++$i;
             print $f $str;
         }
+    } else {
+        delete $names{$key};
     }
 }
 
+print Dumper \@visits;
+
+for $i (1 .. @visits - 1) {
+    my $cur = $visits[$i] || next;
+    my $str = "$cur->{name_id}, str_to_date('$cur->{date}', '%d.%m.%Y')";
+    print $f "insert into `Visits`(`name_id`, `date`) values ($str);\n";
+    $str = "insert into `Treatment`(`visit_id`, `treatment`, `cycle_day`, " .
+        "`endometrium`, `scar`, `fibrosis`, `oncology`, `other_info`) values (" .
+        "$i, '$cur->{treat}', $cur->{circle_day}, '$cur->{endo_w}', " .
+        "$cur->{scar}, $cur->{fibro}, '$cur->{onco}', '$cur->{more}');\n";
+    print $f $str;
+}
+
+print $f "\n\ncommit;";
