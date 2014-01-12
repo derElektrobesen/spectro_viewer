2e5f9f99bce561b3b6f8f787f0bee1bfa054d7ea
derElektrobesen <pberejnoy2005@gmail.com>
2014-01-12 20:56:09 +0400

    Completer fixed

--- forms/savedialog.py
+++ forms/savedialog.py
@@ -1,4 +1,4 @@
-from PyQt4.QtGui import QMainWindow, QStandardItemModel, QStandardItem, QMessageBox
+from PyQt4.QtGui import QMainWindow, QMessageBox, QCompleter
 from PyQt4.QtCore import QObject, pyqtSlot, SIGNAL, Qt
 from PyQt4.QtSql import QSqlQuery
 from pr_core import translate
@@ -12,75 +12,6 @@ class ReqType:
     select_cards = 0
     select_names = 1
 
-class CompleterText:
-    def __init__(self, parent = None):
-        self.__req = None
-        self.__select = QSqlQuery(DB.con())
-        self.__select.prepare("select id, lastname, name, middlename, card_no from filtered_names")
-        self.__data = None
-
-    def update_data(self, text):
-        if self.__req:
-            p = re.compile('\s+')
-            p.sub(text, ' ')
-            self.__req.bindValue(0, text)
-            self.__req.exec_()
-            self.__req.finish()
-
-            self.__rows = []
-
-            q = self.__select
-            q.exec_()
-            if q.next():
-                self.__data = {
-                    'id': q.value(0),
-                    'lastname': q.value(1),
-                    'name': q.value(2),
-                    'middlename': q.value(3),
-                    'card': q.value(4),
-                }
-                self.set_text()
-            else:
-                self.__data = None
-            q.finish()
-
-    def set_text(self):
-        items = ['lastname', 'name', 'middlename'] if self.__req_type == ReqType.select_names else ['card']
-        text = ''
-        for key in items:
-            text += self.__data[key] + " "
-        self.__data['text'] = text[:-1]
-
-    def set_request(self, req_type):
-        self.__req = QSqlQuery(DB.con())
-        text = None
-        if req_type == ReqType.select_cards:
-            text = 'call search_card(?)'
-            self.__keys = ['lastname', 'name', 'middlename']
-        elif req_type == ReqType.select_names:
-            text = 'call search_name(?)'
-            self.__keys = ['card']
-        if text:
-            self.__req.prepare(text)
-            self.__req_type = req_type
-
-    def get_text(self):
-        return self.__data and self.__data['text']
-
-    def get_id(self):
-        return self.__data and self.__data['id']
-
-    def get_other_text(self):
-        t = ''
-        if self.__data:
-            for key in self.__keys:
-                t += self.__data[key] + " "
-            t = t[:-1]
-        return t
-
-    def get_data(self):
-        return self.__data
-
 class SaveDialog(QMainWindow, Ui_SaveDialog):
     def __init__(self, parent = None, collection = None):
         QMainWindow.__init__(self, parent)
@@ -88,6 +19,7 @@ class SaveDialog(QMainWindow, Ui_SaveDialog):
         self.__collection = collection
         self.__do_text_search = True
         self.__visit_id = 0
+        self.__names = {}
 
         self.set_measure_box_value()
 
@@ -97,8 +29,6 @@ class SaveDialog(QMainWindow, Ui_SaveDialog):
 
     def connect_slots(self):
         QObject.connect(self.measures_box, SIGNAL("currentIndexChanged(int)"), self.on_index_changed)
-        QObject.connect(self.name_edt, SIGNAL("textChanged(QString)"), self.on_main_text_changed)
-        QObject.connect(self.card_no_edt, SIGNAL("textChanged(QString)"), self.on_main_text_changed)
 
     def prepare_queries(self):
         def f(q):
@@ -139,13 +69,22 @@ class SaveDialog(QMainWindow, Ui_SaveDialog):
         self.progressBar.setValue(val)
 
     def set_completers(self):
-        m = CompleterText()
-        m.set_request(ReqType.select_names)
-        self.name_edt_lines = m
+        c = QCompleter(self.get_names_list(), self)
+        c.setCaseSensitivity(Qt.CaseInsensitive)
+        self.name_edt.setCompleter(c)
 
-        m = CompleterText()
-        m.set_request(ReqType.select_cards)
-        self.card_no_lines = m
+    def get_names_list(self):
+        q = QSqlQuery(DB.con())
+        q.prepare("call select_names()")
+        q.exec_()
+        q.prepare("select lastname, name, middlename, card_no, id from filtered_names")
+        q.exec_()
+        r = []
+        while (q.next()):
+            t = q.value(0) + " " + q.value(1) + " " + q.value(2)
+            r.append(t)
+            self.__names[t.lower()] = {'card': q.value(3), 'id': q.value(4),}
+        return r
 
     def save_graph(self, cli_id, text, is_intact):
         q = self.__queries['add_visit']
@@ -230,30 +169,6 @@ class SaveDialog(QMainWindow, Ui_SaveDialog):
             self.cur_measure_wgt.set_graph("{measure}-{graph}".format(**index), gr)
             self.cur_measure_wgt.render()
 
-    @pyqtSlot('QString')
-    def on_main_text_changed(self, text):
-        if not self.__do_text_search:
-            self.__do_text_search = True
-            return
-
-        edt = self.sender()
-        if edt.is_backspace_pressed():
-            return
-
-        ref = {
-            'name_edt': self.name_edt_lines,
-            'card_no_edt': self.card_no_lines,
-        }.get(self.sender().objectName(), None)
-        other = self.name_edt if ref == self.card_no_lines else self.card_no_edt
-        if ref:
-            ref.update_data(text)
-            t = ref.get_text()
-            if t:
-                self.__do_text_search = False
-                other.setText(ref.get_other_text())
-                edt.setText(t)
-                edt.setSelection(len(text), len(t) - len(text))
-
     def incorrect_user(self):
         QMessageBox.critical(self, translate("Error", "Ошибка"),
                 translate("Names_err", "Введены некорректные данные о пациенте (имя и/или номер карты)."))
@@ -270,23 +185,23 @@ class SaveDialog(QMainWindow, Ui_SaveDialog):
         return self.do_save(True)
 
     def do_save(self, is_intact):
-        cli_id = None
-        for confirmator, edt in (
-                (self.name_edt_lines, self.name_edt),
-                (self.card_no_lines, self.card_no_edt)):
-            if len(edt.text()) == 0:
-                return self.incorrect_user()
-            confirmator.update_data(edt.text())
-            if not cli_id:
-                cli_id = confirmator.get_id()
-            elif confirmator.get_id() != cli_id or not cli_id:
-                return self.incorrect_user()
+        text = self.name_edt.text().lower()
+        if text not in self.__names:
+            return self.incorrect_user()
+        cli_id = self.__names[text]['id']
         text = self.pnt_edt.text().strip()
         if not len(text):
             return self.incorrect_pnt()
-
         self.__visit_id = self.save_graph(cli_id, text, is_intact)
-
+        
     @pyqtSlot()
     def on_close_btn_pressed(self):
         self.save_extra_info()
+
+    @pyqtSlot()
+    def on_name_edt_editingFinished(self):
+        text = self.name_edt.text().lower()
+        if text in self.__names:
+            self.card_no_edt.setText(self.__names[text]['card'])
+        else:
+            self.card_no_edt.clear()
